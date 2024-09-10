72db83282f29eb132ba7f15392e5fd2af71dabed
RianKwong <hskwongaa@connect.ust.hk>
2024-09-10 23:47:40 +0800

    Add 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -10,6 +10,7 @@ public class mainApp1 {
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program ..Starting...");
         System.out.println("1 + 1 = "+2);
+        System.out.println("1 - 1 = "+0);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
